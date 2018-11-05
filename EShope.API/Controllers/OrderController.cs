316aa19b02d21786f1bf523f8465758a710a4372
abdo.emad <abdo.emad01@gmail.com>
2018-11-05 15:14:12 +0200

    add order user controllers

---
+++ EShope.API/Controllers/OrderController.cs
@@ -0,0 +1,57 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.OData;
+using Microsoft.Azure.Mobile.Server;
+using EShope.API.DataObjects;
+using EShope.API.Models;
+using System;
+
+namespace EShope.API.Controllers
+{
+    //[RoutePrefix("tables/Order/{userId}")]
+    public class OrderController : TableController<Order>
+    {
+        protected override void Initialize(HttpControllerContext controllerContext)
+        {
+            base.Initialize(controllerContext);
+            EShopeMobileServiceContext context = new EShopeMobileServiceContext();
+            DomainManager = new EntityDomainManager<Order>(context, Request);
+        }
+
+        // GET tables/Order
+        //[Route("tables/Order/{userId}")]
+        public IQueryable<Order> GetAllOrder([FromUri]Guid userId)
+        {
+            return Query().Where(o => o.UserId == userId);
+        }
+
+        // GET tables/Order/48D68C86-6EA6-4C25-AA33-223FC9A27959
+        public SingleResult<Order> GetOrder(string id)
+        {
+            return Lookup(id);
+        }
+
+        // PATCH tables/Order/48D68C86-6EA6-4C25-AA33-223FC9A27959
+        public Task<Order> PatchOrder(string id, Delta<Order> patch)
+        {
+             return UpdateAsync(id, patch);
+        }
+
+        // POST tables/Order
+        //[Route("tables/Order/{userId}")]
+        public async Task<IHttpActionResult> PostOrder(Order item)
+        {
+            //item.UserId = userId;
+            Order current = await InsertAsync(item);
+            return CreatedAtRoute("Tables", new { id = current.Id }, current);
+        }
+
+        // DELETE tables/Order/48D68C86-6EA6-4C25-AA33-223FC9A27959
+        public Task DeleteOrder(string id)
+        {
+             return DeleteAsync(id);
+        }
+    }
+}
---
+++ EShope.API/Controllers/UsersController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using EShope.API.DataObjects;
+using EShope.API.Models;
+
+namespace EShope.API.Controllers
+{
+    public class UsersController : ApiController
+    {
+        private EShopeMobileServiceContext db = new EShopeMobileServiceContext();
+
+        // GET: api/Users
+        public IQueryable<User> GetUsers()
+        {
+            return db.Users;
+        }
+
+        // GET: api/Users/5
+        [ResponseType(typeof(User))]
+        public async Task<IHttpActionResult> GetUser(Guid id)
+        {
+            User user = await db.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+
+        // PUT: api/Users/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutUser(Guid id, User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != user.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Users
+        [ResponseType(typeof(User))]
+        public async Task<IHttpActionResult> PostUser(User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Users.Add(user);
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (UserExists(user.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = user.Id }, user);
+        }
+
+        // DELETE: api/Users/5
+        [ResponseType(typeof(User))]
+        public async Task<IHttpActionResult> DeleteUser(Guid id)
+        {
+            User user = await db.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            db.Users.Remove(user);
+            await db.SaveChangesAsync();
+
+            return Ok(user);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool UserExists(Guid id)
+        {
+            return db.Users.Count(e => e.Id == id) > 0;
+        }
+    }
+}
