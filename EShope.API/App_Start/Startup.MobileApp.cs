06a99c7019e6d55dd2d2ed8ac3b6f14523ba0903
abdo.emad <abdo.emad01@gmail.com>
2019-02-18 09:56:39 +0200

    migrate .net framework to 4.7.2

--- EShope.API/App_Start/Startup.MobileApp.cs
+++ EShope.API/App_Start/Startup.MobileApp.cs
@@ -69,7 +69,7 @@ namespace EShope.API
             mobileAppConfiguration.ApplyTo(config);
 
             // Use Entity Framework Code First to create database tables based on your DbContext
-            //Database.SetInitializer(new MobileServiceInitializer());
+            Database.SetInitializer(new MobileServiceInitializer());
 
             //var migrator = new DbMigrator(new Migrations.Configuration());
             //migrator.Update();
