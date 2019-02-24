a2df3bf2cd168a6e4b88e073a0c9def63100ac14
abdo.emad <abdo.emad01@gmail.com>
2019-02-24 17:16:53 +0200

    try custom render

---
+++ EShope/EShope/Controls/MyEntry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace EShope.Controls
+{
+    public class MyEntry : Entry
+    {
+        public MyEntry() : base()
+        {
+            this.BackgroundColor = Color.Red;
+        }
+    }
+}
