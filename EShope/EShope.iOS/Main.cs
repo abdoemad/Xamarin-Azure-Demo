e95fe87c3d65f9545a05eeb47c2b33dce0dc6729
abdo.emad <abdo.emad01@gmail.com>
2019-03-02 14:02:55 +0200

    add try catch to ios main method

--- EShope/EShope.iOS/Main.cs
+++ EShope/EShope.iOS/Main.cs
@@ -12,9 +12,16 @@ namespace EShope.iOS
         // This is the main entry point of the application.
         static void Main(string[] args)
         {
-            // if you want to use a different Application Delegate class from "AppDelegate"
-            // you can specify it here.
-            UIApplication.Main(args, null, "AppDelegate");
+            try
+            {
+                // if you want to use a different Application Delegate class from "AppDelegate"
+                // you can specify it here.
+                UIApplication.Main(args, null, "AppDelegate");
+            }
+            catch (Exception ex)
+            {
+
+            }
         }
     }
 }
