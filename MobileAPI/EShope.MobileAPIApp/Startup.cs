1043b6203a72703df0dacb8c949bbc9285f27da2
abdo.emad <abdo.emad01@gmail.com>
2019-03-18 10:18:40 +0200

    add new azure mobile app

---
+++ MobileAPI/EShope.MobileAPIApp/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(EShope.MobileAPIApp.Startup))]
+
+namespace EShope.MobileAPIApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureMobileApp(app);
+        }
+    }
+}
